b2d96cb34bc06fa3292478c8c661ef293b44b89c
RhydhamP <rhydham.p@medkart.in>
2024-01-18 19:11:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // int days = 30;
+    // String name = "Rhydham";
+    // double pi = 3.14;
+    // bool isMale = true;
+    // num temp = 30.5;
+
+    // var day = "Tuesday";
+    // cons pi = 3.14;
+    // final
+
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      // initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
+                // height: 600,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Welcome",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "Username",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        print("Rhydham");
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.yellowAccent,
+                          minimumSize: Size(120, 40)),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
