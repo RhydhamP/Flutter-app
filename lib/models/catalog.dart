0f66e9e6b4ea2e43d61794e1a2a663c2f76bdf40
RhydhamP <rhydham.p@medkart.in>
2024-01-23 16:14:28 +0530

    1

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,10 +1,22 @@
-class Item {
-  final String id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
-  
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
-}
+// class Item {
+//   final num id;
+//   final String title;
+//   final String name;
+//   final num price;
+//   final String imageUrl;
+//   final String desc;
+
+//   Item({this.id, this.title, this.name, this.price, this.imageUrl, this.desc});
+// }
+
+// final products = [
+//   Item(
+//       id: 0,
+//       title: "Amiral",
+//       name: "Chair",
+//       price: 85000,
+//       imageUrl:
+//           "https://pohjanmaan-prod.s3.amazonaws.com/uploads/product/product_image/317/amiral_1_labrador_1_main_image.png",
+//       desc:
+//           "Individually bench-made by Scandinavian craftsmen. Seat Frame constructed of select SOLID SPRUCE with finger joints and no-zag springs (at least four per seat). Back parts constructed with Mortise & Tenon design for extraordinary durability. Exterior sides and back fully padded.")
+// ];
